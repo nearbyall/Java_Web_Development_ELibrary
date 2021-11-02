786f32418e86349ccdb957f26eaa80ceb902f227
Vladislav Melnikov <notzloyhuman@gmail.com>
2021-11-02 07:56:44 +0300

    Main.java update

--- src/by/epamtc/melnikov/elibrary/main/Main.java
+++ src/by/epamtc/melnikov/elibrary/main/Main.java
@@ -1,27 +1,8 @@
 package by.epamtc.melnikov.elibrary.main;
 
-import java.util.List;
-
-import by.epamtc.melnikov.elibrary.bean.Book;
-import by.epamtc.melnikov.elibrary.dao.DAOProvider;
-import by.epamtc.melnikov.elibrary.dao.StorageDAO;
-import by.epamtc.melnikov.elibrary.dao.exception.DAOException;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		DAOProvider provider = DAOProvider.getInstantce();
-		StorageDAO storageDAO = provider.getStorageDAO();
-		try {
-			List<Book> books = storageDAO.readBooks();
-			storageDAO.writeBooks(books);
-		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
 		
 		
 	}
