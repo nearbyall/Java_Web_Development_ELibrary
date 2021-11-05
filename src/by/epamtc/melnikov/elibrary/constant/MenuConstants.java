841ce729817f5b53eb4b2a550e4648d348764eff
Vladislav Melnikov <notzloyhuman@gmail.com>
2021-11-05 09:25:00 +0300

    Constants update

---
+++ src/by/epamtc/melnikov/elibrary/constant/MenuConstants.java
@@ -0,0 +1,29 @@
+package by.epamtc.melnikov.elibrary.constant;
+
+public final class MenuConstants {
+
+	private MenuConstants() {}
+	
+	public final static String WELCOME = "\n\tWelcome to ELibrary\t\n";
+	
+	public final static String ENTER_LOGIN = "\nEnter login";
+	
+	public final static String ENTER_PASSWORD = "\nEnter password";
+	
+	public final static String AUTHORIZATION_SUCCES = "\nAuthorization was successful";
+	
+	public final static String USER_MENU = "\n1. Show books"
+			+ "\n2. Find books by title"
+			+ "\n3. Find books by title and author"
+			+ "\n0. Exit";
+	
+	public final static String ADMIN_MENU = "\n1. Add book"
+			+ "\n2. Delete book"
+			+ "\n3. Correct note"
+			+ "\n4. Show books"
+			+ "\n5. Find books by title"
+			+ "\n6. Find books by title and author"
+			+ "\n0. Exit";
+																	
+	
+}
---
+++ src/by/epamtc/melnikov/elibrary/constant/ResponseConstants.java
@@ -0,0 +1,9 @@
+package by.epamtc.melnikov.elibrary.constant;
+
+public final class ResponseConstants {
+
+	private ResponseConstants() {}
+	
+	public final static String AUTHORIZATION_FAIL = "Failed to authorization";
+	
+}
