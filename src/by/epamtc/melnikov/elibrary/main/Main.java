96790cd6d65bdc1a59b7ba703ea44df1f99d7e73
Vladislav Melnikov <notzloyhuman@gmail.com>
2021-11-05 09:21:32 +0300

    Main.java update

--- src/by/epamtc/melnikov/elibrary/main/Main.java
+++ src/by/epamtc/melnikov/elibrary/main/Main.java
@@ -1,9 +1,12 @@
 package by.epamtc.melnikov.elibrary.main;
 
-public class Main {
+import by.epamtc.melnikov.elibrary.controller.view.ClientAction;
 
+public class Main {
+	
 	public static void main(String[] args) {
-		
+
+		ClientAction.start();
 		
 	}
 	
