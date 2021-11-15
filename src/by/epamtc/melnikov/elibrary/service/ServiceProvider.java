958a46fb2bdae75d7a2253e6227983e9ad1d415c
Vladislav Melnikov <notzloyhuman@gmail.com>
2021-11-15 12:52:04 +0300

    ServiceProvider.java update

--- src/by/epamtc/melnikov/elibrary/service/ServiceProvider.java
+++ src/by/epamtc/melnikov/elibrary/service/ServiceProvider.java
@@ -4,16 +4,18 @@ import by.epamtc.melnikov.elibrary.service.impl.StorageServiceImpl;
 import by.epamtc.melnikov.elibrary.service.impl.UserServiceImpl;
 
 public class ServiceProvider {
-
-	private static final ServiceProvider instance = new ServiceProvider();
 	
 	private final UserService userService = new UserServiceImpl();
 	private final StorageService storageService = new StorageServiceImpl();
 	
 	private ServiceProvider() {}
 	
+	private static class ServiceProviderHolder {
+		private final static ServiceProvider instance = new ServiceProvider();
+	}
+	
 	public static ServiceProvider getInstance() {
-		return instance;
+		return ServiceProviderHolder.instance;
 	}
 
 	public UserService getUserService() {
